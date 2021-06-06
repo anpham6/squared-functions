e4df7c6ccabe4e748fd55b490f773702ec48a209
An Pham <anpham6@gmail.com>
2021-06-06 04:56:23 -0700

    Chrome: Asset dependencies are not copied during transformation.

--- src/document/chrome/index.ts
+++ src/document/chrome/index.ts
@@ -263,7 +263,7 @@ function removeCss(document: IChromeDocument, source: string) {
     return source;
 }
 
-function getRelativeUri(manager: IFileManager, cssFile: DocumentAsset, asset: DocumentAsset) {
+function getRelativeUri(cssFile: DocumentAsset, asset: DocumentAsset) {
     if (cssFile.inlineContent) {
         return asset.relativeUri!;
     }
@@ -275,18 +275,6 @@ function getRelativeUri(manager: IFileManager, cssFile: DocumentAsset, asset: Do
     if (cssFile.moveTo) {
         if (cssFile.moveTo === asset.moveTo) {
             assetDir = Document.joinPath(asset.moveTo, asset.pathname);
-        }
-        else {
-            const moveUri = path.join(manager.baseDirectory, cssFile.moveTo, asset.relativeUri!);
-            try {
-                if (!fs.existsSync(moveUri)) {
-                    fs.mkdirpSync(path.dirname(moveUri));
-                    fs.copyFileSync(asset.localUri!, moveUri);
-                }
-            }
-            catch (err) {
-                manager.writeFail(['Unable to copy file', path.basename(moveUri)], err, manager.logType.FILE);
-            }
         }
         fileDir = Document.joinPath(cssFile.moveTo, cssFile.pathname);
     }
@@ -357,7 +345,7 @@ function transformCss(manager: IFileManager, assets: DocumentAsset[], cssFile: D
             const base64 = url.split(',')[1];
             for (const item of assets) {
                 if (item.base64 === base64) {
-                    setOutputUrl(item, getRelativeUri(manager, cssFile, item));
+                    setOutputUrl(item, getRelativeUri(cssFile, item));
                     break;
                 }
             }
@@ -370,7 +358,7 @@ function transformCss(manager: IFileManager, assets: DocumentAsset[], cssFile: D
                     fullUrl = asset.inlineBase64 ||= uuid.v4();
                 }
                 else if (!Document.isFileHTTP(fullUrl) || Document.hasSameOrigin(cssUri, fullUrl)) {
-                    fullUrl = getRelativeUri(manager, mainFile || cssFile, asset);
+                    fullUrl = getRelativeUri(mainFile || cssFile, asset);
                 }
                 else {
                     const pathname = cssFile.pathname;
